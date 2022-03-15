5090ee859ae82a5ed07711baf65aebc67ea72929
LAPTOP-5OHKIJ00\FadliArfans <fadliarfanshakim@gmail.com>
2022-03-15 09:37:32 +0700

    feat : add theme

---
+++ lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color white = const Color(0xFFFFFFFF);
+Color black = const Color(0xFF000000);
+Color green = const Color(0xFF00715D);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+TextStyle kWhiteTextStyle = GoogleFonts.montserrat(color: white);
+TextStyle kBlackTextStyle = GoogleFonts.montserrat(color: black);
+TextStyle kGreenTextStyle = GoogleFonts.montserrat(color: green);
+
+double defaultMargin = 24;
